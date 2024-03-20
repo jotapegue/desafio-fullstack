7016d370fd1f178c3159e07fba0c1c0925d10595
João Pedro Garcia <jotapegue09@gmail.com>
2024-03-20 18:39:14 -0300

    fixing base route

--- frontend/src/utils/category/index.ts
+++ frontend/src/utils/category/index.ts
@@ -2,14 +2,19 @@ import type { CategoryInterface } from "@/intefaces/CategoryInterface"
 import { http } from "@/utils/http/http"
 
 export const fetchCategory = async (params?:object) => {
-  const { data } = await http.get('/public/categories', {params})
+  const { data } = await http.get('/v1/public/categories', {params})
+  return data
+}
+
+export const fetchCategoryAdin = async (params?:object) => {
+  const { data } = await http.get('/v1/admin/categories', {params})
   return data
 }
 
 export const storeCategoryFetch = async (form:CategoryInterface) => {
-  return http.postForm('/admin/categories', form)
+  return http.postForm('/v1/admin/categories', form)
 }
 
 export const deleteCategoryFetch = async (category:CategoryInterface) => {
-  return http.delete(`/admin/categories/${category.id}`)
+  return http.delete(`/v1/admin/categories/${category.id}`)
 }
--- frontend/src/utils/product/index.ts
+++ frontend/src/utils/product/index.ts
@@ -2,20 +2,24 @@ import type { ProductInterface } from "@/intefaces/ProductInterface"
 import { http } from "@/utils/http/http"
 
 export const fetchProduct = async (params?:object) => {
-  const { data } = await http.get('/public/products', {params})
+  const { data } = await http.get('/v1/public/products', {params})
+  return data
+}
+
+export const fetchProductAdmin = async (params?:object) => {
+  const { data } = await http.get('/v1/admin/products', {params})
   return data
 }
 
 export const storeProductFetch = async (form:ProductInterface) => {
-  return http.postForm('/admin/product', form)
+  return http.postForm('/v1/admin/product', form)
 }
 
-
 export const deleteProductFetch = async (product:ProductInterface) => {
-  return http.delete(`/admin/product/${product.id}`)
+  return http.delete(`/v1/admin/product/${product.id}`)
 }
 
 export const updateProductFetch = async (form:ProductInterface) => {
-  return http.patch('/admin/product', form)
+  return http.patch('/v1/admin/product', form)
 }
 
