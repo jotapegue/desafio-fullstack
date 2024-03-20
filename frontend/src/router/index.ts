b1e0ab036e3c602ee1cfc0de562891b68a836667
João Pedro Garcia <jotapegue09@gmail.com>
2024-03-20 17:16:27 -0300

    make routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Store from '@/views/Store.vue'
+import Login from '@/views/Login.vue'
+import Category from '@/views/Category.vue'
+import Product from '@/views/Product.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'store',
+      component: Store
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: Category
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: Product
+    },
   ]
 })
 
